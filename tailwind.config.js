82d84c4b31fe3384c295fd244958b3c989bf711c
Arav Bhardwaj <89596267+aravbhardwaj@users.noreply.github.com>
2023-10-26 23:13:00 +0530

    yeah

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./dist/*.html",
+  "./dist/js/*.js",
+],
   theme: {
     extend: {
       backgroundImage: {
         'herobg': "url('/assets/herobg.png')",
         'prone': "url('/assets/p1.png')",
         'edu' : "url('/assets/education.png')",
+        'tcdox' : "url('/assets/techdox.png')"
       },
       fontFamily:{
         'raleway':['Raleway','sans-serif']
